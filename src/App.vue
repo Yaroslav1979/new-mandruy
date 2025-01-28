9757c7d1a811ce6f8196faf6651232cad3150d7f
Yaroslav1979 <yarico.rv@gmail.com>
2025-01-29 00:03:15 +0200

    apartment

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,65 @@
 <template>
   <div :id="$style.app">
-    <h1> {{title}} </h1>
+    <!-- <h1> {{title}} </h1> -->
     <!-- <mainButton @click="increment" outlined>Click</mainButton> -->
     <!-- <DirectivesTest /> -->
-    <StarRating :rating="4"/>
+    <ApartmentsItem 
+    :descr="apartment.descr"
+    :price="apartment.price"
+    :rating="apartment.rating"
+    imgSrc="https://picsum.photos/id/10/300/300"
+    />
   </div>  
 </template>
 
 <script>
 // import mainButton from './components/mainButton.vue'
 // import DirectivesTest from './components/DirectivesTest.vue'
-import StarRating from './components/StarRating.vue'
+import ApartmentsItem from './components/apartment/ApartmentsItem.vue'
 
 export default {
   name: 'App',
   components: {
     // mainButton ,
     // DirectivesTest,
-    StarRating 
+    ApartmentsItem, 
   },
  
   data() {
     return {
-      amountOfClick: 0,
+      apartment: {
+        id: '3265652',
+        title: 'Lorum jbqw3r',
+        descr: 'loerfqbgerfg bwergh jkwergj regjuhcvm , lwerkghodf. mksldwgo',
+        price: 1258.00,
+        rating: 4.5,
+        lacation: {
+          city: 'Rivne',          
+        },
+        owner: {
+          name: 'Olga',
+          phone: '125-25-2152',
+          email: 'fr@gr.com'
+        },
+      },
     }
   },
-  computed: {
-    title() {
-      return `Amount of clicks ${this.amountOfClick}`
-    }
-  },
-  methods: {
-    increment() {
-      this.amountOfClick += 1
-    }
-  }
+  // computed: {
+  //   title() {
+  //     return `Amount of clicks ${this.amountOfClick}`
+  //   }
+  // },
+  // methods: {
+  //   increment() {
+  //     this.amountOfClick += 1
+  //   }
+  // }
 }
 </script>
 
 <style module>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Montserrat, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
