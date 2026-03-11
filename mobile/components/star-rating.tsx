0d2821138bb06d835dee8111a4b499bd75116df4
Yaroslav1979 <yarico.rv@gmail.com>
2026-03-11 23:20:03 +0200

    placeCategory placeRating

---
+++ mobile/components/star-rating.tsx
@@ -0,0 +1,124 @@
+// import { View, StyleSheet, DimensionValue } from "react-native";
+// import StarEmpty from "../assets/svg/Star_rating-empty.svg";
+// import StarFilled from "../assets/svg/Star_rating-yellow.svg";
+
+// type Props = {
+//   rating?: number;
+//   starLimit?: number;
+// };
+
+// export default function StarRating({ rating = 0, starLimit = 5 }: Props) {
+//   const widthPercent = `${(rating / starLimit) * 100}%` as DimensionValue;
+
+//   return (
+//     <View style={styles.wrapper}>
+//       {/* outlined stars */}
+//       <View style={styles.row}>
+//         {Array.from({ length: starLimit }).map((_, i) => (
+//           <StarEmpty key={i} width={16} height={15} style={styles.star} />
+//         ))}
+//       </View>
+
+//       {/* colored overlay */}
+//       <View style={[styles.overlay, { width: widthPercent }]}>
+//         {Array.from({ length: starLimit }).map((_, i) => (
+//           <StarFilled key={i} width={16} height={15} style={styles.star} />
+//         ))}
+//       </View>
+//     </View>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   wrapper: {
+//     position: "relative",
+//     flexDirection: "row",
+//   },
+
+//   row: {
+//     flexDirection: "row",
+//   },
+
+//   overlay: {
+//     position: "absolute",
+//     flexDirection: "row",
+//     top: 0,
+//     left: 0,
+//     overflow: "hidden",
+//   },
+
+//   star: {
+//     marginHorizontal: 3,
+//   },
+// });
+
+import { View, StyleSheet, DimensionValue } from "react-native";
+import StarEmpty from "../assets/svg/Star_rating-empty.svg";
+import StarFilled from "../assets/svg/Star_rating-yellow.svg";
+
+type Props = {
+  rating?: number;
+  starLimit?: number;
+};
+
+// Винесемо константи, щоб легше було рахувати ширину
+const STAR_SIZE = 16;
+const STAR_MARGIN = 3;
+const FULL_STAR_WIDTH = STAR_SIZE + STAR_MARGIN * 2;
+
+export default function StarRating({ rating = 0, starLimit = 5 }: Props) {
+  // Розраховуємо точну ширину контейнера
+  const containerWidth = FULL_STAR_WIDTH * starLimit;
+  // Відсоток заповнення
+  const widthPercent = `${(rating / starLimit) * 100}%` as DimensionValue;
+
+  return (
+    <View style={[styles.wrapper, { width: containerWidth }]}>
+      {/* Нижні порожні зірки */}
+      <View style={styles.row}>
+        {Array.from({ length: starLimit }).map((_, i) => (
+          <StarEmpty
+            key={i}
+            width={STAR_SIZE}
+            height={15}
+            style={styles.star}
+          />
+        ))}
+      </View>
+
+      {/* Верхній шар із заповненими зірками */}
+      <View style={[styles.overlay, { width: widthPercent }]}>
+        <View style={[styles.row, { width: containerWidth }]}>
+          {Array.from({ length: starLimit }).map((_, i) => (
+            <StarFilled
+              key={i}
+              width={STAR_SIZE}
+              height={15}
+              style={styles.star}
+            />
+          ))}
+        </View>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  wrapper: {
+    position: "relative",
+    // Тепер ширина задається динамічно в inline-стилях
+  },
+  row: {
+    flexDirection: "row",
+  },
+  overlay: {
+    position: "absolute",
+    top: 0,
+    left: 0,
+    bottom: 0,
+    overflow: "hidden", // Саме це обрізає зірки
+  },
+  star: {
+    marginHorizontal: STAR_MARGIN,
+  },
+});
---
+++ mobile/constants/categories.ts
@@ -0,0 +1,17 @@
+import Camp from "../assets/svg/categories/camp.svg";
+import Travel from "../assets/svg/categories/travel.svg";
+import Water from "../assets/svg/categories/water.svg";
+import Monument from "../assets/svg/categories/monument.svg";
+import Museum from "../assets/svg/categories/museum.svg";
+import WaterPond from "../assets/svg/categories/waterPond.svg";
+import Transport from "../assets/svg/categories/transport.svg";
+
+export default [
+  { id: "Табір", title: "Табір", icon: Camp },
+  { id: "Мандрівка", title: "Мандрівка", icon: Travel },
+  { id: "Питна вода", title: "Питна вода", icon: Water },
+  { id: "Пам'ятка", title: "Пам'ятка", icon: Monument },
+  { id: "Музей", title: "Музей", icon: Museum },
+  { id: "Водойма", title: "Водойма", icon: WaterPond },
+  { id: "Автомобіль", title: "Автомобіль", icon: Transport },
+];
